9404e46a5930e3ed20d28e4cd902395d289ec114
earosselot <earosselot@gmail.com>
2021-06-28 10:58:22 -0300

    init

---
+++ src/index.js
@@ -0,0 +1,78 @@
+import {WeatherServices} from "./weather";
+
+// const APIkey = '652e61acc78edad67e8910709ea3d274';
+// let cityName1 = 'Mendoza';
+// Weather(APIkey, cityName1).then((resolve) => console.log(resolve));
+
+
+let cityName = 'Neuquen';
+let weatherServices = new WeatherServices();
+weatherServices.createWeather(cityName).then((resolve) => console.log(resolve));
+
+
+const search = document.getElementById('search');
+const mainField = document.getElementById('main-field');
+
+
+search.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const searchData = new FormData(event.target);
+    const city = searchData.get('city');
+    weatherServices.createWeather(city)
+        .then((data) => {
+            displayData(data, mainField);
+            console.log(data);
+        });
+})
+
+
+// Erases previous data on the App and renders the new data
+function displayData (data, field) {
+    field.innerHTML = '';
+    displayField(data.temp, field);
+}
+
+
+// Creates a div element and displays data within DOMelement
+function displayField (fieldData, DOMelement) {
+    const field = document.createElement('div');
+    field.textContent = fieldData;
+    DOMelement.appendChild(field);
+}
+
+
+const DisplayControl = (() => {
+    return {}
+})();
+
+// Weather function hits the weather api with the parameters passed and returns an object with fields used by the App
+// async function Weather(APIkey, cityName, units = 'metric') {
+//     let rawData = await fetch(
+//         `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIkey}&units=${units}`,
+//         {mode: 'cors'});
+//     let data = await rawData.json();
+//     return GetData(data);
+// }
+
+
+// Get data takes the weather json in and returns an object with only the data required by the app
+// function GetData(json) {
+//     let obj = {};
+//     obj['weather'] = json.weather[0].main;
+//     obj['description'] = json.weather[0].description;
+//     obj['temp'] = json.main.temp;
+//     obj['feelsLike'] = json.main.feels_like;
+//     // json.main.temp_min
+//     // json.main.temp_max
+//     // json.main.pressure
+//     // json.main.humidity
+//     // json.clouds.all  // % of clouds
+//     obj['city'] = json.name;  // city name
+//     // json.sys.country  // country code 'AR', 'GB', 'JP', etc...
+//     // json.sys.sunrise  // sunrise time, unix, UTC
+//     // json.sys.sunset // sunset time, unix, UTC
+//     // json.timezone  // shift in seconds form UTC
+//     return obj;
+// }
+
+
---
+++ src/weather.js
@@ -0,0 +1,62 @@
+
+class WeatherServices {
+
+    async createWeather(cityName) {
+        let data = await this.fetchAPI(cityName);
+        console.log(data);
+        let weather = new Weather(data.name, data.weather[0].main, data.weather[0].description, data.main.temp, data.main.feels_like);
+        return weather
+    };
+
+
+    async fetchAPI (cityName, units = 'metric') {
+        let APIkey = '652e61acc78edad67e8910709ea3d274';
+        try {
+            let rawData = await fetch(
+                `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIkey}&units=${units}`,
+                {mode: 'cors'});
+            let json = await rawData.json();
+            return json;
+        } catch (error) {
+            console.log('error: ')
+            console.log(error);
+        }
+
+    };
+
+    // GetData(json){
+    //     let obj = {};
+    //     obj['weather'] = json.weather.main;
+    //     obj['description'] = json.weather.description;
+    //     obj['temp'] = json.main.temp;
+    //     obj['feelsLike'] = json.main.feels_like;
+    //     // json.main.temp_min
+    //     // json.main.temp_max
+    //     // json.main.pressure
+    //     // json.main.humidity
+    //     // json.clouds.all  // % of clouds
+    //     obj['city'] = json.name;  // city name
+    //     // json.sys.country  // country code 'AR', 'GB', 'JP', etc...
+    //     // json.sys.sunrise  // sunrise time, unix, UTC
+    //     // json.sys.sunset // sunset time, unix, UTC
+    //     // json.timezone  // shift in seconds form UTC
+    //     return obj;
+    // };
+
+}
+
+
+class Weather {
+
+    constructor(_city, _weather, _description, _temp, _feelsLike) {
+        this.city = _city;
+        this.weather = _weather;
+        this.description = _description;
+        this.temp = _temp;
+        this.feelsLike = _feelsLike;
+    }
+
+}
+
+
+export { WeatherServices }
